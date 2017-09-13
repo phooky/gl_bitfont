e5bf2d452e22c4e3ce4b16301f2494ac22975ad2
Adam Mayer <phooky@gmail.com>
2017-09-13 14:21:34 -0400

    basic working terminal

--- src/font_fragment.glsl
+++ src/font_fragment.glsl
@@ -2,14 +2,28 @@
 in vec2 v_tex_coords;
 out vec4 color;
 
+// uniform vec2 cell_size; // for now fake it // everything is an 'a' (97)
 uniform vec4 in_color;
 uniform usampler2D tex;
 
+// Compiling in display size
+uniform uint data[80*24];
+
 void main() {
-    float rv = texture(tex,v_tex_coords).r;
+	vec2 cell_size = vec2(8.0,10.0);
+	float max_char = 128.0;
+
+	vec2 term_pos = vec2(floor(v_tex_coords.x),floor(v_tex_coords.y));
+	uint idx = 80u * uint(term_pos.y) + uint(term_pos.x);
+	float char_idx = float( (data[idx/4u] >> (idx%4u)*8u) & 255u );
+
+	vec2 char_off = v_tex_coords - term_pos;
+	vec2 tex_pos = vec2((char_idx+char_off.x)/max_char, char_off.y);
+    float rv = texture(tex,tex_pos).r;
+    //float rv = texture(tex,vec2(v_tex_coords.x/80.0,v_tex_coords.y/24.0)).r;
     if (rv > 0.0) {
-        color = vec4(in_color.rgb, 1.0);
+        color = vec4(0.0,1.0,0.0, 1.0);
     } else {
-        color = vec4(0.0,0.0,0.0,in_color.a);
+        color = vec4(0.0,0.0,0.2,1.0);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 //! gl_bitfont renders simple, old-school pixel fonts 
 extern crate gl;
 
+mod glutil;
+
 use gl::types::*;
+
+// Shader sources
+static VS_SRC: &'static str = include_str!("font_vertex.glsl");
+static FS_SRC: &'static str = include_str!("font_fragment.glsl");
+
+static mut font_program : Option<GLuint> = None;
 
 /// The BitFont trait includes all the information necessary to
 /// load a bit font into the GL engine. All fonts are presumed
@@ -29,13 +37,114 @@ pub struct LoadedFont {
 
 /// An area to render text into, along with the current contents
 /// of the text
-struct Terminal<'a> {
+pub struct Terminal<'a> {
 	dim : (u8,u8),  /// The dimensions, in characters, of this terminal
 	data : Vec<u8>, /// The data to be displayed
 	font : &'a LoadedFont,
+	vao : GLuint,
+}
+
+impl<'a> Terminal<'a> {
+	pub fn new(dimensions : (u8,u8), font : &'a LoadedFont) -> Terminal {
+		let mut vao : GLuint = 0;
+		let mut vbo : GLuint = 0;
+		let mut data : Vec<u8> = Vec::new();
+		let d = (dimensions.0 as f32, dimensions.1 as f32);
+		data.resize(dimensions.0 as usize * dimensions.1 as usize, 0);
+		let vertices : [GLfloat; 4 * 4] = [
+			1.0 , 1.0,   d.0, 0.0,
+			-1.0, 1.0,   0.0, 0.0, 
+			1.0 ,-1.0,   d.0, d.1,
+			-1.0,-1.0,   0.0, d.1,
+		];
+		unsafe {
+			gl::GenVertexArrays(1, &mut vao);
+			gl::BindVertexArray(vao);
+			gl::GenBuffers(1, &mut vbo);
+			gl::BindBuffer(gl::ARRAY_BUFFER,vbo);
+			gl::BufferData(gl::ARRAY_BUFFER, 4*4*4, 
+				vertices.as_ptr() as *const _, gl::STATIC_DRAW);
+
+
+			let program = font_program.unwrap();
+			gl::UseProgram(program);
+            gl::BindFragDataLocation(program, 0,
+                                     std::ffi::CString::new("color").unwrap().as_ptr());
+			let pos_attrib = glutil::attrib_loc(program,"position");
+            gl::EnableVertexAttribArray(pos_attrib as GLuint);
+            gl::VertexAttribPointer(pos_attrib as GLuint, 2, gl::FLOAT, gl::FALSE,
+                                    4*4, std::ptr::null());
+            let tex_attrib = glutil::attrib_loc(program,"tex_coords");
+            gl::EnableVertexAttribArray(tex_attrib as GLuint);
+            gl::VertexAttribPointer(tex_attrib as GLuint, 2, gl::FLOAT, gl::FALSE,
+                                    4*4, (2*4) as *const _);
+
+		}
+		Terminal {
+			dim : dimensions,
+			data : data,
+			font : font,
+			vao : vao,
+		}
+	}
+	pub fn write(&mut self, x : usize, y : usize, text : &str) {
+		let mut idx = x + y*self.dim.0 as usize;
+		for c in text.bytes() {
+			self.data[idx] = c as u8;
+			idx = idx + 1;
+		}
+	}
+
+	pub fn render(&self) {
+		unsafe {
+			let p = font_program.unwrap();
+			gl::UseProgram(p);
+			gl::ActiveTexture(gl::TEXTURE0);
+			gl::BindTexture(gl::TEXTURE_2D,self.font.gl_texture);
+			gl::BindVertexArray(self.vao);
+			// Set uniform for text?
+			let c_str = std::ffi::CString::new("data".as_bytes()).unwrap();
+			let loc = gl::GetUniformLocation(p,c_str.as_ptr());
+			gl::Uniform1uiv(loc,80*24,self.data.as_ptr() as *const _);
+			gl::DrawArrays(gl::TRIANGLE_STRIP, 0, 4);
+		}
+	}
+}
+
+struct EmbeddedFont {
+	cell_size : (u8, u8),
+	intercell : (u8, u8),
+	bounds : (u8, u8),
+	texture : &'static [u8],
+}
+
+impl<'a> BitFont<'a> for EmbeddedFont {
+	fn cell_size_px(&self) -> (u8, u8) { self.cell_size }
+	fn intercell_px(&self) -> (u8, u8) { self.intercell }
+	fn bounds(&self) -> (u8,u8) { self.bounds }
+	fn texture(&self) -> &'a [u8] { self.texture }
+}
+
+fn get_osborne_font() -> EmbeddedFont {
+	EmbeddedFont {
+		cell_size : (8,10),
+		intercell : (0,0),
+		bounds : (0,128),
+		texture : include_bytes!("fonts/Osborne_I.charrom"),
+	}
+}
+
+pub fn osborne_font() -> LoadedFont {
+	load_font(get_osborne_font())
 }
 
 pub fn load_font<'a, T : BitFont<'a> >(font : T) -> LoadedFont {
+	unsafe {
+		if font_program == None {
+			font_program = Some(glutil::build_program(VS_SRC, FS_SRC)
+				.expect("Failed to create font shader program"));
+		}
+	}
 	let cell_size = font.cell_size_px();
 	let bounds = font.bounds();
 	let char_count = bounds.1 - bounds.0;
